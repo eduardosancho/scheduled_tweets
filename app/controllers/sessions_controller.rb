d994182dc76b9b70a74a17759c07fad0228515f1
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-16 23:46:10 -0600

    Create functional sign_in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Succesfully logged in!"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Succesfully logged out!"
