9c2a31ac0de8b17d15f173e2da20c22ad860ce3a
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-17 21:18:28 -0600

    Create db register on Connect Twitter btn click

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,20 @@
 class OmniauthCallbacksController < ApplicationController
   def twitter
-    render plain: 'success'
+    # This cmd prints the entire response from the auth method
+    # Rails.logger.info auth
+
+    # auth.info only works if you have an elevated developer account https://developer.twitter.com/en/portal/products/elevated
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      image: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
+    redirect_to root_path, notice: 'Successfully linked Twitter account.'
+  end
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
