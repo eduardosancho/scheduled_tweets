a5c09a4b5f3a407c49d4ef59898fb94dc3b0f6b5
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-22 16:26:34 -0600

    Add create tweet functionality

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,15 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tweet was scheduled successfully."
+    else
+      render :new
+    end
+  end
   
   def destroy
     @twitter_account.destroy
@@ -19,5 +28,9 @@ class TweetsController < ApplicationController
   
   def set_twitter_account
     @twitter_account = Current.user.twitter_accounts.find(params[:id])
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
   end
 end
