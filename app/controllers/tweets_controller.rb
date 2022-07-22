75556e26216fb35d5560493da080afbc99780647
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-07-22 16:27:01 -0600

    Run linters

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :set_twitter_account, only: [:show, :edit, :update, :destroy]
+  before_action :set_twitter_account, only: %i[show edit update destroy]
 
   def index
     @tweets = Current.user.tweets
@@ -13,19 +13,19 @@ class TweetsController < ApplicationController
   def create
     @tweet = Current.user.tweets.new(tweet_params)
     if @tweet.save
-      redirect_to tweets_path, notice: "Tweet was scheduled successfully."
+      redirect_to tweets_path, notice: 'Tweet was scheduled successfully.'
     else
       render :new
     end
   end
-  
+
   def destroy
     @twitter_account.destroy
     redirect_to twitter_accounts_path, notice: "Successfully unlinked #{@twitter_account.username} account."
   end
 
   private
-  
+
   def set_twitter_account
     @twitter_account = Current.user.twitter_accounts.find(params[:id])
   end
